695ea74fb7fe1992d16ab129b30c8f6eba948d0d
edwardo37 <cameronschultz421@gmail.com>
2025-04-25 21:09:28 -0400

    Add option to choose number of players, with memory-safe player list

--- blackjack.cpp
+++ blackjack.cpp
@@ -4,26 +4,62 @@
 #include <algorithm>
 #include <random>
 
+#include <limits>
+
 #include "card.h"
 #include "player.h"
 
-// main is here for testing, until finalized
+// For safe(er) memory management with a dynamic list of players
+class PlayerList {
+public:
+    explicit PlayerList(const int numPlayers) {
+        for (int i = 0; i < numPlayers; ++i) {
+            contents.push_back(new Player);
+        }
+    }
+
+    ~PlayerList() {
+        for (const auto p : contents) {
+            delete p;
+        }
+    }
+
+    Player* operator[](const int index) const {
+        return contents[index];
+    }
+
+    std::vector<Player*> contents;
+};
+
+
 int main() {
     Deck deck;
     deck.shuffle();
-    deck._print();
 
-    Player player1;
+    std::vector<Player> players;
 
-    // player1.drawCard(deck);
-    // player1.drawCard(deck);
+    // Pick number of players
+    int numPlayers = 0;
+    while (!numPlayers) {
+        std::cout << "Enter number of players: ";
+        std::cin >> numPlayers;
 
-    player1.printHand();
+        // Check if valid
+        if (!std::cin || numPlayers < 2 || numPlayers > 7) {
+            std::cout << "Number of players must be between 2 and 7." << std::endl;
 
-    player1.discardCard(0);
+            // Reset and continue
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            numPlayers = 0;
+        }
+    }
 
-    deck._print();
-    player1.printHand();
+    // Create all players
+    PlayerList BJ_Players(numPlayers);
+    Player Dealer;
+
+
 
     return 0;
 }
